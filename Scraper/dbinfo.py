c1e9dda4a65843c841638e68e530d1989057bab3
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-02-22 20:56:21 +0000

    First bike scraper and database connection test

---
+++ Scraper/dbinfo.py
@@ -0,0 +1,5 @@
+JCKEY = "JC_KEY"
+URL = "https://api.jcdecaux.com/vls/v1/stations"
+CONTRACT = "dublin"
+
+DATABASE_URL = "mysql+pyDATABASE_URLring-ring-bike"
---
+++ Scraper/scraper-bike.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from dbinfo import *
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+engine = create_engine(DATABASE_URL)
+
+def store(text):
+    with engine.connect() as connection:
+        query = text("SELECT * FROM ringringbikes.stations")
+        result = connection.execute(query)
+        print(result)
+
+
+def main():
+    r = requests.get(URL, params={"contract":CONTRACT, "apiKey": JCKEY})
+    #store(json.loads(r.text))
+    print(json.loads(r.text))
+
+
+main()
+store()
