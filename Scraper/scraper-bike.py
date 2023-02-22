87c95074ea6facddb6821a91d13c9fd52bde5b09
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-02-22 22:04:11 +0000

    Create stations table

--- Scraper/scraper-bike.py
+++ Scraper/scraper-bike.py
@@ -12,16 +12,26 @@ def test_connection():
         for line in result:
             print(line)
 
+def create_stations(stations):
+    with engine.connect() as connection:
+        for station in stations:
+            print(station)
+            vals = (int(station.get("number")), station.get("name"), station.get("address"), int(station.get("available_bike_stands")))
+            result = connection.execute("INSERT INTO `ringringbikes`.`stations` (`station_id`, `name`, `address`, `total_bike_stands`) VALUES ('%s', '%s', '%s', '%s')", vals)
+
 
 def store(stations):
-    for station in stations:
-        print(station)
-        vals = (int(station.get("number")), int(station.get("last_update")), int(station.get("available_bikes")), int(station.get("available_bike_stands")), station.get("status"))
-        engine.execute("INSERT INTO `ringringbikes`.`station_availability` (`station_id`, `last_update`, `available_bikes`, `available_bike_stands`, `status`) VALUES (%s, %s, %s, %s, %s)", vals)
+    with engine.connect() as connection:
+        for station in stations:
+            print(station)
+            vals = (int(station.get("number")), int(station.get("last_update")), int(station.get("available_bikes")), int(station.get("available_bike_stands")), station.get("status"))
+            result = connection.execute("INSERT INTO `ringringbikes`.`station_availability` (`station_id`, `last_update`, `available_bikes`, `available_bike_stands`, `status`) VALUES (%s, %s, %s, %s, %s)", vals)
 
 def main():
     r = requests.get(URL, params={"contract":CONTRACT, "apiKey": JCKEY})  
-    store(json.loads(r.text))
+    stations = json.loads(r.text)
+    create_stations(stations)
+    store(stations)
     #print(json.loads(r.text))
 
 
