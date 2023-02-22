75718eb751719577c4a707a52176fa9037971c90
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-02-22 22:53:28 +0000

    Scraping with session and errorhandling

--- Scraper/scraper-bike.py
+++ Scraper/scraper-bike.py
@@ -11,9 +11,9 @@ Base = automap_base()
 engine = create_engine(DATABASE_URL)
 
 # reflect the tables
-Base.prepare(autoload_with=engine)
-Station_Availability_table = Base.classes.ringringbikes.station_availability
-Stations_table = Base.classes.ringringbikes.stations
+Base.prepare(autoload_with=engine, schema="ringringbikes")
+Station_Availability_table = Base.classes.station_availability
+Stations_table = Base.classes.stations
 
 def test_connection():
     with Session(engine) as session:
@@ -27,13 +27,10 @@ def create_stations(stations):
             #print(station)
             try:
                 session.add(Stations_table(station_id=int(station.get("number")), name=station.get("name"), address=station.get("address"), total_bike_stands=int(station.get("available_bike_stands"))))
-                #vals = (int(station.get("number")), station.get("name"), station.get("address"), int(station.get("available_bike_stands")))
-                #result = session.execute("INSERT INTO `ringringbikes`.`stations` (`station_id`, `name`, `address`, `total_bike_stands`) VALUES (%s, %s, %s, %s)", vals)
-            except:
-                session.rollback()
-                raise
-            else:
                 session.commit()
+            except IntegrityError:
+                session.rollback()
+                
 
 def time_to_datetime(time):
     # Format "1000-01-01 00:00:00"
@@ -45,15 +42,20 @@ def store(stations):
     with Session(engine) as session:
         for station in stations:
             #print(station)
-            vals = (int(station.get("number")), time_to_datetime(int(station.get("last_update"))), int(station.get("available_bikes")), int(station.get("available_bike_stands")), station.get("status"))
+            try:
+                session.add(Station_Availability_table(station_id=int(station.get("number")), last_update=time_to_datetime(int(station.get("last_update"))), available_bikes=int(station.get("available_bikes")), available_bike_stands=int(station.get("available_bike_stands")), status=station.get("status")))
+                session.commit()
+            except IntegrityError:
+                session.rollback()
+            #vals = (int(station.get("number")), time_to_datetime(int(station.get("last_update"))), int(station.get("available_bikes")), int(station.get("available_bike_stands")), station.get("status"))
             
-            result = session.execute("INSERT INTO `ringringbikes`.`station_availability` (`station_id`, `last_update`, `available_bikes`, `available_bike_stands`, `status`) VALUES (%s, %s, %s, %s, %s)", vals)
+            #result = session.execute("INSERT INTO `ringringbikes`.`station_availability` (`station_id`, `last_update`, `available_bikes`, `available_bike_stands`, `status`) VALUES (%s, %s, %s, %s, %s)", vals)
 
 def main():
     r = requests.get(URL, params={"contract":CONTRACT, "apiKey": JCKEY})  
     stations = json.loads(r.text)
-    create_stations(stations)
-    #store(stations)
+    #create_stations(stations)
+    store(stations)
     #print(json.loads(r.text))
 
 
