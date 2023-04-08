204d69d1be2825cd9602630ea79fe0d01a7c4bd5
tlopesx <tlopes123x@gmail.com>
2023-04-08 13:36:37 +0100

    Draw routes between two bike stations (static for now)

--- web/static/script/context.js
+++ web/static/script/context.js
@@ -5,5 +5,10 @@ let context = {
     markers: [],         
     applicationTime: new Date(Date.now())
   };
+
+let routeParams = {
+  origin: { 'Lat' : 53.3547, 'Long' :	-6.27868 },
+  destination: { 'Lat' : 53.3398, 'Long' :	-6.25199 },
+}
   
-export {context}
+export { context, routeParams }
