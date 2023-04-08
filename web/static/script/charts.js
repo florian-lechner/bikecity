c90059a26d7d3ac20561a7debd9b0597eec1b79e
tlopesx <tlopes123x@gmail.com>
2023-04-08 02:14:12 +0100

    Just tweaking one last line of code

--- web/static/script/charts.js
+++ web/static/script/charts.js
@@ -5,16 +5,18 @@ var weeklyChart;
 
 function createCharts(stationAvailability, historicalStationData) {
     
+    // Add close event to X
     addChartWindowCloseEvent();
+    
     // Set the title of the window
     if (context.openChartWindow == undefined) {
         context.openChartWindow = document.getElementById("chart-window");
     }
     context.openChartWindow.children[1].innerHTML = stationAvailability.name;
 
+    // Variable for open hours
     const openHours = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-    
-    var openTime = openHours.map((hour) => {
+    let openTime = openHours.map((hour) => {
         if (hour < 12) {
           return String(hour + ":00 AM");
         } else if (hour === 12) {
@@ -23,26 +25,29 @@ function createCharts(stationAvailability, historicalStationData) {
           return String(hour - 12 + ":00 PM");
         }
       });
-    
+      // Variable to hold days of the week
       const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 
     // Get all values that correspond to the selected day
-    var singleDay = historicalStationData.filter(
+    let singleDay = historicalStationData.filter(
         entry => entry.day == days[context.applicationTime.getDay()] && openHours.includes(entry.hour));
     singleDay = singleDay.sort((a, b) => a.hour - b.hour);
 
     // Generate array for each hour of the day
     const dailyData = singleDay.map((hour) => hour.average_bike_availability);
 
-    // Display the daily chart data
+    // If there is already a chart, destroy it
     if (dailyChart != undefined) {
         dailyChart.destroy();
     }
-     dailyChart = displayChart('daily', openTime, 'Available Bikes', dailyData);
+    // Display the daily chart
+    dailyChart = displayChart('daily', openTime, 'Available Bikes', dailyData);
 
-
+    // Declare array to store the average per day of the week
     let weeklyData = []
+    // Get the average
+
     days.forEach(day => {
         let bikes = 0;
         let count = 0;
@@ -54,16 +59,20 @@ function createCharts(stationAvailability, historicalStationData) {
         });
         weeklyData.push(bikes / count);
     })
+
+    // If there is already a chart, destroy it
     if (weeklyChart != undefined) {
         weeklyChart.destroy();
     }
+    // Display the weekly chart
     weeklyChart = displayChart('weekly', days, 'Available Bikes', weeklyData);
     
+    // Open the chart window
     openChartWindow(stationAvailability);
 }
 
 function displayChart(chartId, chartLabels, chartTitle, chartData) {
-    var chart = new Chart(
+    const chart = new Chart(
         document.getElementById(chartId),
         {
             type: 'bar',
@@ -98,6 +107,7 @@ function closeChartWindow() {
             context.openChartWindow.style.right = pos + "px";
         }
     }
+    context.openChartWindow = undefined;
 }
 
 function addChartWindowCloseEvent() {
