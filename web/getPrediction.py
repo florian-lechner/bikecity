fcc17ad6cc766084ddeb2f37a499dc76d2db063f
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-04-19 16:01:43 +0100

    Change back

--- web/getPrediction.py
+++ web/getPrediction.py
@@ -6,33 +6,7 @@ import numpy as np
 
 import pickle
 
-with open('../Prediction_Model/predictionModels.pkl', 'rb') as file:
-    prediction_models = pickle.load(file)
 
-def get_available_bike_prediction(station_id, date, temperature, pressure, humidity, clouds, precipitation_value, precipitation_probability):
-    """ date as string: '02-05-2023, 23:59' """
-    # time conversion:
-
-    # hours * 12 + minutes//5 * 1
-    time = int(date[11:13]) * 12 + int(date[14:16])//5
-    # day conversion:
-    d = pd.to_datetime(date[:10], format="%d-%m-%Y")
-    day = d.dayofweek
-
-    input_value = np.array([[temperature, pressure, humidity, clouds, precipitation_value, precipitation_probability, time, day]])
-    input_df = pd.DataFrame(input_value, columns=['temperature', 'pressure', 'humidity', 'clouds', 'precipitation_value', 'precipitation_probability', 'time', 'day'])
-    
-    pred = float(prediction_models[station_id][0](input_df))
-    pred_bike_chance = round(float(prediction_models[station_id][2](input_df)) * 99)
-    pred_stand_chance = round(float(prediction_models[station_id][3](input_df)) * 99)
-
-
-    pred_bikes = round(pred * prediction_models[station_id][1])
-    pred_stations = prediction_models[station_id][1]-pred_bikes
-
-    return pred_bikes, pred_stations, pred_bike_chance, pred_stand_chance
-
-"""
 def load_station_model(station_id):
     with open(f'/var/www/bikecity/ring-ring-bike/Prediction_Model/station_models/predictionModels_{station_id}.pkl', 'rb') as file:
         return pickle.load(file)
